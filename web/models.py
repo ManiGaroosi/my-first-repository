bffcb31f138b369d774ae887ccefa360f9f4f131
mani garoosi <manigaroosicoder@gmail.com>
2023-03-13 18:52:53 +0330

    I'm add Income on models

--- web/models.py
+++ web/models.py
@@ -6,3 +6,15 @@ class Expense(models.Model):
     date = models.DateField()
     amount = models.BigIntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.BigIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
